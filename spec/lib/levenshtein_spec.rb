b73b42920881fb1a482a1e9f606d2f3e8b3aaeca
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-01-19 15:06:21 +0200

    Add spec for Levenshtein.default_strategy

--- spec/lib/levenshtein_spec.rb
+++ spec/lib/levenshtein_spec.rb
@@ -4,4 +4,10 @@ require "spec_helper"
 
 describe StringMetric::Levenshtein do
   it_behaves_like "Levenshtein Distance"
+
+  describe '#default_strategy' do
+    it "has a default strategy" do
+      expect(described_class.default_strategy).to be
+    end
+  end
 end
