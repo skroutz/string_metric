7f34d894f35ab7ec5bbc6dcdde857511ad0f50fc
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-01-20 01:27:10 +0200

    Use fixtures in benchmarks

--- benchmarks/levenshtein.rb
+++ benchmarks/levenshtein.rb
@@ -2,24 +2,38 @@ require "benchmark"
 require "string_metric"
 require "text"
 require "pry"
+require "csv"
 
 Benchmark.bmbm(7) do |x|
 
-  iterations = 1_000
-
+  iterations = 3_000
   options = {}
 
+  fixtures = []
+  CSV.foreach("spec/fixtures/levenshtein.csv") do |row|
+    from, to, _ = row
+
+    fixtures.push [from.to_s.strip, to.to_s.strip]
+  end
+
   StringMetric::Levenshtein::STRATEGIES.each do |strategy, implementation|
+    next if strategy == :recursive
+
     x.report("#{implementation.to_s} implementation") do
       iterations.times do |i|
-        implementation.distance("kitten", "sitting", options)
+
+        fixtures.each do |from, to|
+          implementation.distance(from, to, options)
+        end
       end
     end
   end
 
   x.report("Text::Levenshtein implementation") do
     iterations.times do |i|
-      Text::Levenshtein.distance("kitten", "sitting")
+      fixtures.each do |from, to|
+        Text::Levenshtein.distance(from, to)
+      end
     end
   end
 end
