1944a564b94d88ea3e61b638783b68762559c61d
Aggelos Avgerinos <avgerinos@skroutz.gr>
2014-01-22 09:29:42 +0200

    Remove useless to_a from full_matrix Levenshtein

--- lib/string_metric/levenshtein/iterative_with_full_matrix.rb
+++ lib/string_metric/levenshtein/iterative_with_full_matrix.rb
@@ -14,11 +14,11 @@ module StringMetric
           [0] * (from.size + 1)
         end
 
-        (1..from.size).to_a.each { |j| d[0][j] = j }
-        (1..to.size).to_a.each { |i| d[i][0] = i }
+        (1..from.size).each { |j| d[0][j] = j }
+        (1..to.size).each { |i| d[i][0] = i }
 
-        (1..from.size).to_a.each do |j|
-          (1..to.size).to_a.each do |i|
+        (1..from.size).each do |j|
+          (1..to.size).each do |i|
             if from[j-1] == to[i-1]
               d[i][j] = d[i -1][j-1]
             else
